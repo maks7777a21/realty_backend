d837766552a63a445cb221aefc75fb256b0bc55c
mopanasenko <opanasenkomax@gmail.com>
2018-11-06 00:22:54 +0200

    module complete

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const User  =  require('../models/user');
-const jwt = require('jsonwebtoken');
-
 const mongoose = require('mongoose');
-const db = 'mongodb://admin:passwordlinux1qaz@ds151513.mlab.com:51513/realtydb';
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const db = "mongodb://testuser:testpw@ds123136.mlab.com:23136/eventsdb";
+// mongoose.Promise = global.Promise;
 
-mongoose.connect(db, err => {
-    if (err){
-        console.error(`Error! ${err}`);
-    } else{
-        console.log('connected to db =)');
+mongoose.connect(db, function (err) {
+    if (err) {
+        console.error('Error! ' + err);
+    } else {
+        console.log('Connected to mongodb');
     }
 });
 
-
 function verifyToken(req, res, next) {
     if (!req.headers.authorization) {
-        return res.status(401).send('Unauthorized request');
+        return res.status(401).send('Unauthorized request')
     }
-    let token = req.headers.authorization.split(' ')[1]
+    let token = req.headers.authorization.split(' ')[1] ; 
     if (token === 'null') {
         return res.status(401).send('Unauthorized request');
     }
-    let payload = jwt.verify(token, 'secretKey')
+    let payload = jwt.verify(token, 'secretKey');
     if (!payload) {
         return res.status(401).send('Unauthorized request');
     }
@@ -69,7 +68,7 @@ router.get('/events', (req, res) => {
             "description": "lorem ipsum",
             "date": "2012-04-23T18:25:43.511Z"
         }
-    ]
+    ];
     res.json(events);
 });
 
@@ -111,46 +110,42 @@ router.get('/special', verifyToken, (req, res) => {
             "description": "lorem ipsum",
             "date": "2012-04-23T18:25:43.511Z"
         }
-    ]
+    ];
     res.json(specialEvents);
-});
+})
 
-router.get('/', (req, res) => {
-    res.send('From API route');
-});
-
-router.post('/register', (req, res)=>{
+router.post('/register', (req, res) => {
     let userData = req.body;
     let user = new User(userData);
-    user.save((error, registeredUser) => {
-        if (error){
-            console.log(error);
+    user.save((err, registeredUser) => {
+        if (err) {
+            console.log(err);
         } else {
-            let payload = { subject: registeredUser._id }
+            let payload = { subject: registeredUser._id };
             let token = jwt.sign(payload, 'secretKey');
-            res.status(200).send({token});
+            res.status(200).send({ token });
         }
-    });
-});
+    })
+})
 
 router.post('/login', (req, res) => {
     let userData = req.body;
-
-    User.findOne({email: userData.email}, (error, user)=> {
-        if (error){
-            console.log('error' + error);
+    User.findOne({ email: userData.email }, (err, user) => {
+        if (err) {
+            console.log(err);
         } else {
-            if (!user){
-                res.status(401).send('invalid Email');
-            } else if (user.password !== userData.password){
-                res.status(401).send('Invalid password');
-            } else {
-                let payload = { subject: user._id }
-                let token = jwt.sign(payload, 'secretKey');
-                res.status(200).send({ token });
-            }
+            if (!user) {
+                res.status(401).send('Invalid Email');
+            } else
+                if (user.password !== userData.password) {
+                    res.status(401).send('Invalid Password');
+                } else {
+                    let payload = { subject: user._id }
+                    let token = jwt.sign(payload, 'secretKey');
+                    res.status(200).send({ token });
+                }
         }
-    });
+    })
 });
 
 module.exports = router;
