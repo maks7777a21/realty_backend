307a6d460ba048efe4405780f859fdc60f0781fd
mopanasenko <opanasenkomax@gmail.com>
2018-11-05 22:27:54 +0200

    added token verification

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User  =  require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
 const db = 'mongodb://admin:passwordlinux1qaz@ds151513.mlab.com:51513/realtydb';
@@ -125,7 +126,9 @@ router.post('/register', (req, res)=>{
         if (error){
             console.log(error);
         } else {
-            res.status(200).send(registeredUser);
+            let payload = { subject: registeredUser._id }
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({token});
         }
     });
 });
@@ -142,7 +145,9 @@ router.post('/login', (req, res) => {
             } else if (user.password !== userData.password){
                 res.status(401).send('Invalid password');
             } else {
-                res.status(200).send(user);
+                let payload = { subject: user._id }
+                let token = jwt.sign(payload, 'secretKey');
+                res.status(200).send({ token });
             }
         }
     });
